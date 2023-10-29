fe2e951a55760233e6d0a2fbeb477ecaf33b4a99
Nephyrr <lucasperez702@gmail.com>
2023-10-29 19:40:01 +0100

    add projets screens & grid

--- application/app/projets/page.tsx
+++ application/app/projets/page.tsx
@@ -17,14 +17,25 @@ export default function Projets() {
                     <section id='projets' className='w-full pb-[-50px] bg-[url("/bg1.svg")] bg-cover bg-right lg:bg-center overflow-hidden'>
                         <div className='max-w-screen-lg mx-auto flex flex-col'>
                             <h2 className='font-montserrat text-xl text-center mt-10'>Projets - Coming soon</h2>
-                            <div className="grid grid-flow-row-dense grid-cols-3 gap-4 my-10">
-                                <div className="col-span-2 h-[200px] bg-zinc-500 rounded-md p-2">01</div>
-                                <div className="h-[200px] bg-zinc-500 rounded-md p-2">02</div>
-                                <div className="h-[200px] bg-zinc-500 rounded-md p-2">03</div>
-                                <div className="col-span-2 h-[200px] bg-zinc-500 rounded-md p-2">04</div>
-                                <div className="h-[200px] bg-zinc-500 rounded-md p-2">05</div>
-                                <div className="h-[200px] bg-zinc-500 rounded-md p-2">06</div>
-                                <div className="h-[200px] bg-zinc-500 rounded-md p-2">07</div>
+                            <div className="grid grid-flow-row-dense grid-cols-1 lg:grid-cols-2 gap-4 my-10">
+                                <div className="bg-[url('/projets/woa-1.png')] bg-cover h-[300px] bg-zinc-500 rounded-md p-2 relative hover:scale-105 transition-transform ease-in-out duration-300">
+                                    <div className='absolute left-0 bottom-0 mb-4 ml-2 text-white font-bold bg-light-dark rounded-md p-2 mt-2'>Walk of Art</div>
+                                </div>
+                                <div className="bg-[url('/projets/swoaper.png')] bg-cover h-[300px] bg-zinc-500 rounded-md p-2 relative hover:scale-105 transition-transform ease-in-out duration-300">
+                                    <div className='absolute left-0 bottom-0 mb-4 ml-2 text-white font-bold bg-light-dark rounded-md p-2 mt-2'>Swoaper</div>
+                                </div>
+                                <div className="bg-[url('/projets/portfolio-2023.png')] bg-cover h-[300px] bg-zinc-500 rounded-md p-2 relative hover:scale-105 transition-transform ease-in-out duration-300">
+                                    <div className='absolute left-0 bottom-0 mb-4 ml-2 text-white font-bold bg-light-dark rounded-md p-2 mt-2'>Portfolio 2023</div>
+                                </div>
+                                <div className="bg-[url('/projets/company-finder.png')] bg-cover h-[300px] bg-zinc-500 rounded-md p-2 relative hover:scale-105 transition-transform ease-in-out duration-300">
+                                    <div className='absolute left-0 bottom-0 mb-4 ml-2 text-white font-bold bg-light-dark rounded-md p-2 mt-2'>Company Finder</div>
+                                </div>
+                                <div className="bg-[url('/projets/carpool-1.png')] bg-cover h-[300px] bg-zinc-500 rounded-md p-2 relative hover:scale-105 transition-transform ease-in-out duration-300">
+                                    <div className='absolute left-0 bottom-0 mb-4 ml-2 text-white font-bold bg-light-dark rounded-md p-2 mt-2'>Carpool</div>
+                                </div>
+                                <div className="bg-[url('/projets/outil-cr-vlabs.png')] bg-cover h-[300px] bg-zinc-500 rounded-md p-2 relative hover:scale-105 transition-transform ease-in-out duration-300">
+                                    <div className='absolute left-0 bottom-0 mb-4 ml-2 text-white font-bold bg-light-dark rounded-md p-2 mt-2'>Outil de compte rendu</div>
+                                </div>
                             </div>
                         </div>
                     </section>
